359deb888b6da7946f12c7fefe67a3dc3bb71542
Michael Quarne <mikedq1225@gmail.com>
2020-08-14 17:38:15 -0700

    added some images and styled a little

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "../styles/Home.css"
 
 export default (props) => {
 	return (
-		<div>
-			<h1>Avengers Initiative</h1>
+		<div className="home">
+			<h1>Avengers Initiative Portal</h1>
 			<Link to="/apply">
-				<button type="button">Apply to the Avengers</button>
+				<button type="button">Apply to the New Avengers</button>
 			</Link>
 		</div>
 	)
